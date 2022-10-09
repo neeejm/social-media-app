48b08e58967d6ac7118027187a5f66907efb425a
Neeejm <nejmzakaria@gmail.com>
2022-10-09 11:51:12 +0100

    add find alll endpoint

--- apps/posts/src/main/java/com/neeejm/posts/models/Post.java
+++ apps/posts/src/main/java/com/neeejm/posts/models/Post.java
@@ -1,24 +1,46 @@
 package com.neeejm.posts.models;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Version;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Document
 @Data
-@EqualsAndHashCode(callSuper = false)
 @Builder
-public class Post extends BaseEntity {
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
 
-    // @Id
-    // private String id;
+    @Id
+    private String id;
 
     private String title;
 
     private String content;
 
     private Integer views;
+
+    @Version
+    private Integer version;
+
+    @CreatedDate
+    @Builder.Default
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @LastModifiedDate
+    @Builder.Default
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime modifiedAt = LocalDateTime.now();
 }
